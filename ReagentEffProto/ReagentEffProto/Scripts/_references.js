355513d9925f6a28c690d9de6e2d404670620a73
gmarkc <greg.cockerham@gmail.com>
2016-04-03 13:40:48 -0400

    adding

---
+++ ReagentEffProto/ReagentEffProto/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="angular.js" />
+/// <reference path="angular-mocks.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="jquery-1.9.1.js" />
+/// <reference path="../viewmodel/reagentefficiencyreportviewmodel.js" />
